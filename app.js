2e1a7a6adffd2f98af3c8640a6ce59f9d6c652d1
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-30 11:45:40 -0500

    Surprise me button with RGB gen

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ d3.select('.remove')
         .remove();
     });
 
+//Surprise Me Button
+d3.select('.lucky')
+    .on('click', function(){
+      d3.selectAll('.note')
+        .style('color', randomRGB)
+        .style('background-color', randomRGB)
+        .style('border', '8px solid black')
+    })
+
 
 //Selects the new note input field and appends content to the DOM
 d3.select("#new-note")
@@ -17,3 +26,10 @@ d3.select("#new-note")
           .text(input.property('value'));
       input.property('value', '');
     });
+
+const randomRGB = () => {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
