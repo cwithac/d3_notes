5962e32a17383f9a7011aa054dd46fd822538bb1
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-30 11:33:04 -0500

    Code commenting for note submission

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+//Selects the new note input field and appends content to the DOM
 d3.select("#new-note")
     .on('submit', function() {
-      d3.event.preventDefault();
-      var input = d3.select('input');
+      d3.event.preventDefault(); //Prevents page reload on 'submit'
+      const input = d3.select('input');
       d3.select("#notes")
         .append('p')
           .classed('note', true)
