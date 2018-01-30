d3958a6f6ffaad5d046c347aa11875fcfda18ec2
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-30 11:51:45 -0500

    Preview functionality

--- app.js
+++ app.js
@@ -14,19 +14,36 @@ d3.select('.lucky')
         .style('border', '8px solid black')
     })
 
+//Global Variables
+const input = d3.select('input');
+const preview = d3.select('.preview');
 
 //Selects the new note input field and appends content to the DOM
 d3.select("#new-note")
     .on('submit', function() {
       d3.event.preventDefault(); //Prevents page reload on 'submit'
-      const input = d3.select('input');
       d3.select("#notes")
         .append('p')
           .classed('note', true)
           .text(input.property('value'));
       input.property('value', '');
+      setPreview('') //hides after preview completion
     });
 
+//Preview Input
+input.on('input', function() {
+  const note = d3.event.target.value; //Value of input
+  setPreview(note);
+});
+
+//Set Preview Helper
+function setPreview(val) {
+  preview.text(val)
+      .classed('hide', val === ''); //hides if no value
+}
+
+
+//RGB Color Gen
 const randomRGB = () => {
   let r = Math.floor(Math.random() * 256);
   let g = Math.floor(Math.random() * 256);
